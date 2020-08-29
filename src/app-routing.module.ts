e8db6acf7de3c52a45124de155ba89cd35fecfa7
Linda Krueger <lkrueger17@gmail.com>
2020-08-29 15:50:41 +0200

    added ng-bootstrap

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { ComponentInteractionComponent } from './components/component-interaction/component-interaction.component';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
 
 const routes: Routes = [
   { path: 'databinding', component: DatabindingComponent },
   { path: 'component-interaction', component: ComponentInteractionComponent },
+  { path: 'bootstrap', component: BootstrapComponent },
   // { path: 'path2/:id', component: SecondComponent },
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
   // { path: '**', component: PageNotFoundComponent }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './components/app/app.component';
 import { ComponentInteractionComponent } from './components/component-interaction/component-interaction.component';
 import { ChildComponent } from './components/component-interaction/child/child.component';
 import { DatabindingComponent } from './components/databinding/databinding.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentInteractionComponent,
     ChildComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    BootstrapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
